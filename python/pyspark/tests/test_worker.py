467ef31dc3af117885d25f806120ead01dc5dcf6
chasingegg <gaoc96@qq.com>
2022-01-06 16:21:48 +0800

    remove again

--- python/pyspark/tests/test_worker.py
+++ python/pyspark/tests/test_worker.py
@@ -186,13 +186,13 @@ class WorkerTests(ReusedPySparkTestCase):
             self.assertRegex(str(e), "exception with 中")
 
 
-class WorkerReuseTest(PySparkTestCase):
-    def test_reuse_worker_of_parallelize_range(self):
-        rdd = self.sc.parallelize(range(20), 8)
-        previous_pids = rdd.map(lambda x: os.getpid()).collect()
-        current_pids = rdd.map(lambda x: os.getpid()).collect()
-        for pid in current_pids:
-            self.assertTrue(pid in previous_pids)
+# class WorkerReuseTest(PySparkTestCase):
+#     def test_reuse_worker_of_parallelize_range(self):
+#         rdd = self.sc.parallelize(range(20), 8)
+#         previous_pids = rdd.map(lambda x: os.getpid()).collect()
+#         current_pids = rdd.map(lambda x: os.getpid()).collect()
+#         for pid in current_pids:
+#             self.assertTrue(pid in previous_pids)
 
 
 @unittest.skipIf(
